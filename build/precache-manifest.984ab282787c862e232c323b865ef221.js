7cd4703e0b0ed1f9a837433d43f7580cd48c4ddc
rambej0201 <1503010201@svuca.edu>
2020-02-22 18:51:30 -0700

    initial commit

---
+++ build/precache-manifest.984ab282787c862e232c323b865ef221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9792709c1fb2ea6bf0c464d1f7d992bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd93f64b2cf85a6e6da",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "13876ad73957f6dce6d3",
+    "url": "/static/js/2.f8c00595.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f8c00595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd93f64b2cf85a6e6da",
+    "url": "/static/js/main.2e6d0af1.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
